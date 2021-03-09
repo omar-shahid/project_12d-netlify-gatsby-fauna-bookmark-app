48623cc6a5bd4466d286a8a4f271fe0ffd6384ff
Omar <omarshahid160@gmail.com>
2021-03-09 00:23:46 -0500

    Create UI with Chakra

--- src/components/EditModal.js
+++ src/components/EditModal.js
@@ -16,6 +16,7 @@ import {
   Box,
 } from "@chakra-ui/react";
 import { EditIcon } from "@chakra-ui/icons";
+import { Formik, Form, Field } from "formik";
 
 const EditModal = (props) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -24,35 +25,58 @@ const EditModal = (props) => {
       <EditIcon fontSize={18} cursor="pointer" onClick={onOpen} />
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody>
-            <Box my={5}>
-              <FormControl id="name">
-                <FormLabel>Name</FormLabel>
-                <Input type="text" value={props.name} />
-              </FormControl>
-              <FormControl id="url">
-                <FormLabel>URL</FormLabel>
-                <Input type="text" value={props.url} />
-              </FormControl>
-              <FormControl id="email">
-                <FormLabel>Description</FormLabel>
-                <Textarea type="text" value={props.description} />
-              </FormControl>
-            </Box>
-          </ModalBody>
+        <Formik
+          initialValues={{
+            name: props.name,
+            url: props.url,
+            description: props.description,
+          }}
+          onSubmit={(values) => {
+            console.log("WOrlding here!!", values);
+            return props
+              .onEdit(props.id, ...Object.values(values))
+              .then(() => onClose());
+          }}
+        >
+          {({ handleSubmit }) => (
+            <Form>
+              <ModalContent>
+                <ModalHeader>Modal Title</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                  <Box my={5}>
+                    <FormControl id="name">
+                      <FormLabel>Name</FormLabel>
+                      <Field type="text" as={Input} name="name" />
+                    </FormControl>
+                    <FormControl id="url">
+                      <FormLabel>URL</FormLabel>
+                      <Field type="text" as={Input} name="url" />
+                    </FormControl>
+                    <FormControl id="email">
+                      <FormLabel>Description</FormLabel>
+                      <Field type="text" as={Textarea} name="description" />
+                    </FormControl>
+                  </Box>
+                </ModalBody>
 
-          <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
-              Edit
-            </Button>
-            <Button variant="ghost" onClick={onClose}>
-              Cancel
-            </Button>
-          </ModalFooter>
-        </ModalContent>
+                <ModalFooter>
+                  <Button
+                    colorScheme="blue"
+                    mr={3}
+                    type="submit"
+                    onClick={handleSubmit}
+                  >
+                    Edit
+                  </Button>
+                  <Button variant="ghost" onClick={onClose}>
+                    Cancel
+                  </Button>
+                </ModalFooter>
+              </ModalContent>
+            </Form>
+          )}
+        </Formik>
       </Modal>
     </>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,67 +10,122 @@ import {
   FormLabel,
   Input,
   Textarea,
+  Button,
 } from "@chakra-ui/react";
 import { DeleteIcon } from "@chakra-ui/icons";
 import EditModal from "./../components/EditModal";
 import axios from "axios";
+import { Form, Formik, Field } from "formik";
 
 const { useState, useEffect } = React;
 
 const IndexPage = () => {
-  const [bookmarks, setBookmarks] = useState({});
+  const [bookmarks, setBookmarks] = useState([]);
   useEffect(() => {
     axios
       .post("/.netlify/functions/allBookmarks")
       .then((res) => res.data)
       .then((data) => {
         console.log("data", data);
-        setBookmarks(data?.data?.allItems.data);
+        setBookmarks(data?.data?.allItems.data.slice().reverse());
       });
   }, []);
-  console.log(bookmarks);
+
+  function onEdit(id, name, url, description) {
+    console.log("EDIABLE>>>", { id, name, url, description });
+    return axios
+      .post("/.netlify/functions/editBookmark", { id, name, url, description })
+      .then((res) => res.data)
+      .then(() => {
+        const newList = [...bookmarks];
+        const editableBookmark = newList.find((el) => el._id);
+        if (!editableBookmark) return;
+        const editableBookmarkIndex = newList.indexOf(editableBookmark);
+        newList[editableBookmarkIndex] = { _id: id, name, url, description };
+        setBookmarks(newList);
+      });
+  }
+
+  function deleteItem(id) {
+    return axios
+      .post("/.netlify/functions/deleteItem", { id })
+      .then((res) => res.data)
+      .then(() => {
+        console.log(id);
+        setBookmarks((p) => p.filter((el) => el._id !== id));
+      });
+  }
   return (
     <Container mt={5}>
       <Heading>Book mark App</Heading>
 
-      <Box my={5}>
-        <FormControl id="name">
-          <FormLabel>Name</FormLabel>
-          <Input type="text" />
-        </FormControl>
-        <FormControl id="url">
-          <FormLabel>URL</FormLabel>
-          <Input type="text" />
-        </FormControl>
-        <FormControl id="email">
-          <FormLabel>Description</FormLabel>
-          <Textarea type="text" />
-        </FormControl>
-      </Box>
+      <Formik
+        initialValues={{
+          name: "",
+          url: "",
+          description: "",
+        }}
+        onSubmit={(values, { resetForm }) => {
+          return axios
+            .post("/.netlify/functions/createBookmark", values)
+            .then((res) => res.data)
+            .then((id) => {
+              const newBookmark = { _id: id, ...values };
+              setBookmarks((p) => [newBookmark].concat(p));
+              resetForm();
+            });
+        }}
+      >
+        <Form>
+          <Box my={5}>
+            <FormControl id="name">
+              <FormLabel>Name</FormLabel>
+              <Field type="text" as={Input} name="name" />
+            </FormControl>
+            <FormControl id="url">
+              <FormLabel>URL</FormLabel>
+              <Field type="text" as={Input} name="url" />
+            </FormControl>
+            <FormControl id="email">
+              <FormLabel>Description</FormLabel>
+              <Field as={Textarea} name="description" />
+            </FormControl>
+            <Button colorScheme="blue" mr={3} type="submit">
+              Create bookmark
+            </Button>
+          </Box>
+        </Form>
+      </Formik>
 
       <Stack spacing={8} mt={4}>
         {bookmarks?.map((item) => (
-          <a key={item.name} href={item.url}>
-            <Box p={5} shadow="md" borderWidth="1px">
-              <Flex>
+          <Box p={5} key={item._id} shadow="md" borderWidth="1px">
+            <Flex>
+              <a key={item.name} href={item.url}>
                 <Box>
                   <Heading fontSize="xl">{item.name}</Heading>
                   <Heading size="sm" color="GrayText" fontWeight="light">
                     {item.url}
                   </Heading>
                 </Box>
-                <Box ml="auto">
-                  <DeleteIcon fontSize={18} cursor="pointer" />
-                  <EditModal
-                    name={item.name}
-                    url={item.url}
-                    description={item.description}
-                  />
-                </Box>
-              </Flex>
-              <Text mt={4}>{item.description}</Text>
-            </Box>
-          </a>
+              </a>
+              <Box ml="auto">
+                <DeleteIcon
+                  fontSize={18}
+                  cursor="pointer"
+                  onClick={() => deleteItem(item._id)}
+                />
+                <EditModal
+                  id={item._id}
+                  name={item.name}
+                  url={item.url}
+                  description={item.description}
+                  onEdit={onEdit}
+                />
+              </Box>
+            </Flex>
+            <Text mt={4}>{item.description}</Text>
+          </Box>
         ))}
       </Stack>
     </Container>
